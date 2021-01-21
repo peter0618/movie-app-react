1093b6236dbc9e574fc0629ee8121d4683f5c857
peter0618 <peterchoi0618@gmail.com>
2021-01-22 02:35:38 +0900

    state 기본 및 Component LifeCycle 예제 작성

--- src/App.js
+++ src/App.js
@@ -1,68 +1,44 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I love {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} width="200px" alt="" />
-    </div>
-  )
-}
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    console.log('constructor()');
+  }
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-}
-
-const foodILike = [
-  {
-    id: 1,
-    name: 'kimchi',
-    image: 'https://cdn.imweb.me/thumbnail/20200415/bb2adfaee6702.png',
-    // rating: 5,
-  },
-  {
-    id: 2,
-    name: 'kimbob',
-    image:
-      'https://homecuisine.co.kr/files/attach/images/140/001/083/558d170258752df2dd76bef00861497f.JPG',
-    rating: 4.5,
-  },
-  {
-    id: 3,
-    name: 'gogi',
-    image:
-      'https://static.hubzum.zumst.com/hubzum/2018/12/05/13/38035029939e44f3b76c6a0ddae80842.jpg',
-    rating: 4.9,
-  },
-  {
-    id: 4,
-    name: 'hamburger',
-    image:
-      'https://image.chosun.com/sitedata/image/202001/13/2020011303147_0.jpg',
-    rating: 4.8,
-  },
-]
-
-// listItem 에 key 가 없으면 안된다는 오류가 떠서, id를 지정해주고 key 로 넘겨줌.
-function App() {
-  return (
-    <div>
-      {foodILike.map((food) => {
-        return (
-          <Food
-            name={food.name}
-            picture={food.image}
-            key={food.id}
-            rating={food.rating}
-          />
-        )
-      })}
-    </div>
-  )
+  // state 는 dynamic data 를 담는 Object 입니다.
+  // state 를 setState 함수를 이용해 변경하면 render() 함수를 호출합니다.
+  state = {
+    count: 0,
+  };
+  add = () => {
+    console.log('add');
+    this.setState((prevState) => {
+      return { count: prevState.count + 1};
+    })
+  };
+  minus = () => {
+    console.log('minus');
+    this.setState((prevState) => {
+      return { count: prevState.count - 1};
+    })
+  }
+  render() {
+    console.log(`render()`);
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+  }
+  componentDidMount() {
+    console.log(`componentDidMount()`);
+  }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log(`componentDidUpdate()`);
+  }
 }
 
 export default App
