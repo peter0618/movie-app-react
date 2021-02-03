363b9f51902047f85320a60201424c15509cd10c
peter0618 <peterchoi0618@gmail.com>
2021-02-04 01:07:09 +0900

    프로젝트 폴더 구조 변경 및 HashRouter 추가

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import './App.css'
 import './Movie.css'
 
 function Movie({ year, title, summary, poster, genres }) {
---
+++ src/routes/Home.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import axios from 'axios'
+import Movie from '../components/Movie'
+import './Home.css'
+
+class Home extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  }
+
+  render() {
+    const { isLoading, movies } = this.state
+    // const mapToComponent = (data) => {
+    //   return data.map((movie) => {
+    //     return <h4 key={movie.id}>{movie.title}</h4>
+    //   })
+    // }
+    // return (
+    //   <div>
+    //     <span>{isLoading ? 'Loading...' : 'We are ready'}</span>
+    //     {mapToComponent(movies)}
+    //   </div>
+    // )
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                key={movie.id}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    )
+  }
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      'https://yts-proxy.now.sh/list_movies.json?sort_by=rating'
+    )
+    this.setState({ isLoading: false, movies })
+  }
+
+  componentDidMount() {
+    this.getMovies()
+  }
+}
+
+export default Home
