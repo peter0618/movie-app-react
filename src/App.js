212148e3928efc531f14b57953b10f7f8a2d02c2
peter0618 <peterchoi0618@gmail.com>
2021-02-07 01:53:10 +0900

    부모컴포넌트에서 자식컴포넌트로 데이터(props)를 전달하는 예제 작성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import Potato from './Potato'
+
+function Food({favorite}) {
+  return <h3>I love {favorite}</h3>
+}
+
+function Food2(props) {
+  return <h3>I love {props.favorite}</h3>
+}
 
 function App() {
   return (
     <div>
       <h1>Hello!!</h1>
-      <Potato/>
+      <Food favorite="kimchi" />
+      <Food favorite="kimbob" />
+      <Food favorite="gogi" />
+      <Food favorite="hamburger" />
+      <Food2 favorite="hamburger2" />
     </div>
   )
 }
