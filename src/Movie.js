5edad6d64dc61a72f54341cf860728211edf1096
peter0618 <peterchoi0618@gmail.com>
2021-02-08 00:11:07 +0900

    Styles the list

--- src/Movie.js
+++ src/Movie.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './App.css'
+import './Movie.css'
 
 function Movie({ year, title, summary, poster, genres }) {
   return (
@@ -9,14 +10,14 @@ function Movie({ year, title, summary, poster, genres }) {
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <ul className="genres">
+        <ul className="movie__genres">
           {genres.map((genre, index) => (
             <li className="genres__genre" key={index}>
               {genre}
             </li>
           ))}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0, 140)}...</p>
       </div>
     </div>
   )
